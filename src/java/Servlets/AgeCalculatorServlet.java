8b3674e0dad44001464e6371ed45e36ab9cd6800
Home <Home@DESKTOP-1LBKKKP>
2023-01-27 14:20:54 -0700

    Week 3 Lab Created doGet() and doPost() Methods and updated JSP's

--- src/java/Servlets/AgeCalculatorServlet.java
+++ src/java/Servlets/AgeCalculatorServlet.java
@@ -1,5 +1,11 @@
 package Servlets;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +16,23 @@ package Servlets;
  *
  * @author Home
  */
-public class AgeCalculatorServlet {
+public class AgeCalculatorServlet extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+                .forward(request, response);
+        
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String firstNumber = request.getParameter("firstNum");
+        String secondNumber = request.getParameter("secondNum");
+        
+    }
     
 }
